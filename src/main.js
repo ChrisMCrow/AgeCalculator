cac77e53bab53def7ea7cd1ff6772856bf82d1e9
Chris Crow <chrismcrow@gmail.com>
2018-10-12 13:37:14 -0700

    begin front end. Build form

--- src/main.js
+++ src/main.js
@@ -4,5 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
-
+  $("form").submit(function(event) {
+    event.preventDefault();
+    let birthdate = $("#birthdate").val();
+    let gender = $("input[name='gender']:checked").val();
+    let origin = $("#origin option:selected").val();
+    let newUser = new AgeCalculator(birthdate, gender, origin);
+  });
 });
